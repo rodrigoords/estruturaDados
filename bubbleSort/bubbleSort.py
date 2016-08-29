40884f32d6c0bb6b91466037e6d3f82281d68d51
Rodrigo Sene <rodrigoords@hotmail.com>
2016-08-29 00:08:20 -0300

    melhoria de codigo

--- bubbleSort/bubbleSort.py
+++ bubbleSort/bubbleSort.py
@@ -1,36 +1,40 @@
 import random
 
 ARRAY_SIZE = 20
-hasAlter = True
-bubbleIteration = [None]*ARRAY_SIZE
 
-
-def setUp():
-    random.seed()
-    for x in range (0,ARRAY_SIZE):
-        bubbleIteration[x] = random.randint(0,100)
-
-def bubbleSort(bubbleIteration):
-    global hasAlter
-    while hasAlter:
-        hasAlter = False
-        for x in range(0,len(bubbleIteration)-1):
-            if bubbleIteration[x] > bubbleIteration[x+1]:
-                alterValues(bubbleIteration, x, x+1)
-
-
-def alterValues(bubbleIteration, indexA, indexB):
-    global hasAlter
-    aux = bubbleIteration[indexA]
-    bubbleIteration[indexA] = bubbleIteration[indexB]
-    bubbleIteration[indexB] = aux
+class Bubble():
     hasAlter = True
+    bubbleIteration = []
 
+    def __init__(self, arraySize):
+        self.bubbleIteration = [None]*arraySize
+        random.seed()
 
-setUp()
+    def setUp(self):
+        for x in range (0,len(self.bubbleIteration)):
+            self.bubbleIteration[x] = random.randint(0,100)
+
+    def sort(self):
+        while self.hasAlter:
+            self.hasAlter = False
+            for x in range(0,len(self.bubbleIteration)-1):
+                if self.bubbleIteration[x] > self.bubbleIteration[x+1]:
+                    self.alterValues(x, x+1)
+
+    def alterValues(self, indexA, indexB):
+        aux = self.bubbleIteration[indexA]
+        self.bubbleIteration[indexA] = self.bubbleIteration[indexB]
+        self.bubbleIteration[indexB] = aux
+        self.hasAlter = True
+
+    def getBubbleIteration(self):
+        return self.bubbleIteration
+
+bubble = Bubble(10)
+bubble.setUp()
 print "=========== List ==========="
-print bubbleIteration
+print bubble.getBubbleIteration()
 print "=========== executing Bubble Sort ==========="
-bubbleSort(bubbleIteration)
+bubble.sort()
 print "=========== Result after Bubble Sort ==========="
-print bubbleIteration
+print bubble.getBubbleIteration()
